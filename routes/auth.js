11a8be3fcd1bf86a09dc2bc838833bb9e1d65f1c
RQuicho <raymondquicho@gmail.com>
2023-09-07 16:38:52 -0700

    adding routes for auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,30 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const jwt = require("jsonwebtoken");
+const {SECRET_KEY} = require("../config");
+const User = require("../models/user");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+router.post('/login', async (req, res, next) => {
+    try {
+        const {username, password} = req.body;
+        if (await User.authenticate(username, password)) {
+            const token = jwt.sign({username}, SECRET_KEY);
+            await User.updateLoginTimestamp(username);
+            return res.json({message: `Logged in as ${username}`, token});
+        }
+        throw new ExpressError('Invalid username/password', 400);        
+    } catch(e) {
+        return next(e);
+    }
+});
+
+
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -11,3 +33,18 @@
  *
  *  Make sure to update their last-login!
  */
+router.post('/register', async (req, res, next) => {
+    try {
+        const {username} = await User.register(req.body);
+        const token = jwt.sign({username}, SECRET_KEY);
+        await User.updateLoginTimestamp(username);
+        return res.json({message: `Registered as ${username}`, token});
+    } catch(e) {
+        if (e.code === "23505") {
+            return next(new ExpressError('Username taken. Please pick another.', 400));
+        }
+        return next(e);
+    }
+});
+
+module.exports = router;
